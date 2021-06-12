c601761ae0fa74abd7136ae62b3b3e85843ed3e6
Jon Stump <jmstump@gmail.com>
2021-06-12 11:21:49 -0700

    add pseudo code for installs

--- Repos/dotfiles/setup.sh
+++ Repos/dotfiles/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Install regardless of OSTYPE
+
+
 if [[ "$OSTYPE" == "linux-gnu" ]];
 then
   echo "Linux is your OS"
@@ -8,3 +11,24 @@ then
   echo "MacOS is your OS"
   # do setup for Mac
 fi
+
+linux_setup() {
+#Homebrew
+  curl and install brew via their script. &&
+    sudo apt install &&
+    # apps to install via apt
+    &&
+    &&
+    &&
+    &&
+    &&
+    &&
+    &&
+  <commands>
+}
+
+mac_setup() {
+#Homebrew
+  curl and install brew via their script.
+  <commands>
+}
