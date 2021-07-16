81775c04a9947e2cf30c6d56ecd41703c2070c13
Jon Stump <jmstump@gmail.com>
2021-07-16 13:23:55 -0700

    add git cloning dotfiles, change shell to zsh, back up preinstall dotfiles

--- Repos/dotfiles/setup.sh
+++ Repos/dotfiles/setup.sh
@@ -5,14 +5,14 @@ linux_setup() {
 # Apt install
 
 # Add Spotify Repository
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+  curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+  echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # Add Signal Repositroy
-wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+  wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+  cat signal-desktop-keyring.gpg | sudo tee -a /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
 
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
   sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
 
 # Apt update and upgrade
@@ -22,6 +22,33 @@ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] 
   sudo apt install kitty zsh zsh-antigen neofetch neovim imagemagick code docker virtualbox spotify-client signal-desktop -y
 
   sudo apt clean
+
+  #set shell to ZSH
+  sudo chsh -s /bin/zsh
+
+  #make directory for dotfiles and other repos
+
+  mkdir Repos
+  mkdir $HOME/Repos/dotfiles
+
+  #clone git dotfiles repository
+  git clone --bare https://github.com/jonstump/dotfiles.git $HOME/Repos/dotfiles
+
+  #makes config alias for use of the script to setup dotfiles
+  function config {
+    /usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME
+  }
+
+  #make a backup of already existing dotfiles
+  mkdir -p .config-backup && \
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+    xargs -I{} mv {} .config-backup/{}
+
+  #checkout content from bare git repository for dotfiles
+  config checkout
+
+  #set tracked files to no
+  config config --local status.showUntrackedFiles no
 
   brew file install
 }
